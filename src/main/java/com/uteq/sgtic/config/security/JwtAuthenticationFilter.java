ced2d1457461c2384ab43ef8de2ae0253dc0efe3
CarlosMC2004 <cmanotoaa@uteq.edu.ec>
2026-03-19 11:22:08 -0500

    Corrección de los ID en el flujo del proyecto

--- src/main/java/com/uteq/sgtic/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/uteq/sgtic/config/security/JwtAuthenticationFilter.java
@@ -79,10 +79,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         
         filterChain.doFilter(request, response);
     }
-
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        String path = request.getRequestURI();
-        return path.startsWith("/chat-socket");
-    }
 }
